8d02b008b67b80c81a62059c2ca405db34e2d158
Graham Holker <graham.holker@gmail.com>
2015-02-15 18:36:25 -0500

    Bring in Jan 2015 changes from Apple and remove the delay

--- main.c
+++ main.c
@@ -1,48 +1,47 @@
-//
-//	File:		main.c
-//
-//	Contains:	main source code to HID LED test tool
-//
-//	Copyright:	Copyright (c) 2007 Apple Inc., All Rights Reserved
-//
-//	Disclaimer: IMPORTANT:	This Apple software is supplied to you by
-//				Apple Inc. ("Apple") in consideration of your agreement to the
-//				following terms, and your use, installation, modification or
-//				redistribution of this Apple software constitutes acceptance of these
-//				terms.	If you do not agree with these terms, please do not use,
-//				install, modify or redistribute this Apple software.
-//
-//				In consideration of your agreement to abide by the following terms, and
-//				subject to these terms, Apple grants you a personal, non-exclusive
-//				license, under Apple's copyrights in this original Apple software (the
-//				"Apple Software"), to use, reproduce, modify and redistribute the Apple
-//				Software, with or without modifications, in source and/or binary forms;
-//				provided that if you redistribute the Apple Software in its entirety and
-//				without modifications, you must retain this notice and the following
-//				text and disclaimers in all such redistributions of the Apple Software.
-//				Neither the name, trademarks, service marks or logos of Apple Inc.
-//				may be used to endorse or promote products derived from the Apple
-//				Software without specific prior written permission from Apple.	Except
-//				as expressly stated in this notice, no other rights or licenses, express
-//				or implied, are granted by Apple herein, including but not limited to
-//				any patent rights that may be infringed by your derivative works or by
-//				other works in which the Apple Software may be incorporated.
-//
-//				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
-//				MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
-//				THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
-//				FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
-//				OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-//
-//				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
-//				OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//				SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//				INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
-//				MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
-//				AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
-//				STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
-//				POSSIBILITY OF SUCH DAMAGE.
-//
+//     File: main.c
+// Abstract: source code for HID LED test tool
+//  Version: 1.3
+// 
+// Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+// Inc. ("Apple") in consideration of your agreement to the following
+// terms, and your use, installation, modification or redistribution of
+// this Apple software constitutes acceptance of these terms.  If you do
+// not agree with these terms, please do not use, install, modify or
+// redistribute this Apple software.
+// 
+// In consideration of your agreement to abide by the following terms, and
+// subject to these terms, Apple grants you a personal, non-exclusive
+// license, under Apple's copyrights in this original Apple software (the
+// "Apple Software"), to use, reproduce, modify and redistribute the Apple
+// Software, with or without modifications, in source and/or binary forms;
+// provided that if you redistribute the Apple Software in its entirety and
+// without modifications, you must retain this notice and the following
+// text and disclaimers in all such redistributions of the Apple Software.
+// Neither the name, trademarks, service marks or logos of Apple Inc. may
+// be used to endorse or promote products derived from the Apple Software
+// without specific prior written permission from Apple.  Except as
+// expressly stated in this notice, no other rights or licenses, express or
+// implied, are granted by Apple herein, including but not limited to any
+// patent rights that may be infringed by your derivative works or by other
+// works in which the Apple Software may be incorporated.
+// 
+// The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+// MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+// OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+// 
+// IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+// MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+// AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+// STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// 
+// Copyright (C) 2015 Apple Inc. All Rights Reserved.
+// 
 // ****************************************************
 #pragma mark -
 #pragma mark * complation directives *
@@ -117,6 +116,7 @@ static CFMutableDictionaryRef hu_CreateMatchingDictionaryUsagePageUsage(Boolean 
 
 int main(int argc, const char *argv[]) {
 #pragma unused ( argc, argv )
+    IOHIDDeviceRef *tIOHIDDeviceRefs = nil;
 
     // create a IO HID Manager reference
     IOHIDManagerRef tIOHIDManagerRef = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
@@ -147,11 +147,17 @@ int main(int argc, const char *argv[]) {
     CFIndex deviceIndex, deviceCount = CFSetGetCount(deviceCFSetRef);
 
     // allocate a block of memory to extact the device ref's from the set into
-    IOHIDDeviceRef *tIOHIDDeviceRefs = malloc(sizeof(IOHIDDeviceRef) * deviceCount);
-    require(tIOHIDDeviceRefs, Oops);
+    tIOHIDDeviceRefs = malloc(sizeof(IOHIDDeviceRef) * deviceCount);
+    if (!tIOHIDDeviceRefs) {
+        CFRelease(deviceCFSetRef);
+        deviceCFSetRef = NULL;
+        goto Oops;
+    }
 
     // now extract the device ref's from the set
     CFSetGetValues(deviceCFSetRef, (const void **) tIOHIDDeviceRefs);
+    CFRelease(deviceCFSetRef);
+    deviceCFSetRef = NULL;
 
     // before we get into the device loop we'll setup our element matching dictionary
     matchingCFDictRef = hu_CreateMatchingDictionaryUsagePageUsage(FALSE, kHIDPage_LEDs, 0);
@@ -170,8 +176,7 @@ int main(int argc, const char *argv[]) {
                 continue;    // ...skip it
             }
 
-            IOHIDDeviceRef pDevice = tIOHIDDeviceRefs[deviceIndex];
-//            printf("	 device = %p.\n", pDevice);
+            printf("	 device = %p.\n", tIOHIDDeviceRefs[deviceIndex]);
 
             // copy all the elements
             CFArrayRef elementCFArrayRef = IOHIDDeviceCopyMatchingElements(tIOHIDDeviceRefs[deviceIndex],
@@ -201,8 +206,7 @@ int main(int argc, const char *argv[]) {
                     uint32_t usage = IOHIDElementGetUsage(tIOHIDElementRef);
                     IOHIDElementType tIOHIDElementType = IOHIDElementGetType(tIOHIDElementRef);
 
-
-                    printf("		 element = %p (page: %d, usage: %d, type: %d )",
+                    printf("		 element = %p (page: %d, usage: %d, type: %d ).\n",
                             tIOHIDElementRef, usagePage, usage, tIOHIDElementType);
 
                     // get the logical mix/max for this LED element
@@ -216,7 +220,7 @@ int main(int argc, const char *argv[]) {
                     CFIndex tCFIndex = minCFIndex + (device_value % modCFIndex);
                     device_value /= modCFIndex;
 
-                    printf(" -- value = 0x%08lX.\n", tCFIndex);
+                    printf("			 value = 0x%08lX.\n", tCFIndex);
 
                     uint64_t timestamp = 0; // create the IO HID Value to be sent to this LED element
                     IOHIDValueRef tIOHIDValueRef = IOHIDValueCreateWithIntegerValue(kCFAllocatorDefault, tIOHIDElementRef, timestamp, tCFIndex);
@@ -231,24 +235,32 @@ int main(int argc, const char *argv[]) {
                     continue;
                 }
             }
-            next_device:;
             CFRelease(elementCFArrayRef);
+            next_device:;
             continue;
         }
 
         // if we found an LED we'll delay before continuing
-        if (delayFlag) {
-            usleep(500000); // sleep one half second
+//        if (delayFlag) {
+//            usleep(500000); // sleep one half second
+//        }
+
+        // if the mouse is down…
+        if (GetCurrentButtonState()) {
+            break;    // abort pass loop
         }
     }                          // next pass
-
-    if (tIOHIDManagerRef) {
-        CFRelease(tIOHIDManagerRef);
-    }
 
     if (matchingCFDictRef) {
         CFRelease(matchingCFDictRef);
     }
     Oops:;
+    if (tIOHIDDeviceRefs) {
+        free(tIOHIDDeviceRefs);
+    }
+
+    if (tIOHIDManagerRef) {
+        CFRelease(tIOHIDManagerRef);
+    }
     return 0;
 } /* main */
