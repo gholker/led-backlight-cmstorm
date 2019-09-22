752e92a3e084ffdd7b39336368c9cd010c814160
Graham Holker <graham.holker@rossintelligence.com>
2019-09-22 17:41:57 -0400

    When no configuration is found cycle through the configurations and ask the user if the configuration is good. If `y` then save the configuration.

--- main.c
+++ main.c
@@ -71,19 +71,19 @@
 // ----------------------------------------------------
 // function to create a matching dictionary for usage page & usage
 static CFMutableDictionaryRef hu_CreateMatchingDictionaryUsagePageUsage(Boolean isDeviceNotElement,
-        UInt32 inUsagePage,
-        UInt32 inUsage) {
+                                                                        UInt32 inUsagePage,
+                                                                        UInt32 inUsage) {
     // create a dictionary to add usage page / usages to
     CFMutableDictionaryRef result = CFDictionaryCreateMutable(kCFAllocatorDefault,
-            0,
-            &kCFTypeDictionaryKeyCallBacks,
-            &kCFTypeDictionaryValueCallBacks);
-
+                                                              0,
+                                                              &kCFTypeDictionaryKeyCallBacks,
+                                                              &kCFTypeDictionaryValueCallBacks);
+    
     if (result) {
         if (inUsagePage) {
             // Add key for device type to refine the matching dictionary.
             CFNumberRef pageCFNumberRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &inUsagePage);
-
+            
             if (pageCFNumberRef) {
                 if (isDeviceNotElement) {
                     CFDictionarySetValue(result, CFSTR(kIOHIDDeviceUsagePageKey), pageCFNumberRef);
@@ -91,11 +91,11 @@ static CFMutableDictionaryRef hu_CreateMatchingDictionaryUsagePageUsage(Boolean 
                     CFDictionarySetValue(result, CFSTR(kIOHIDElementUsagePageKey), pageCFNumberRef);
                 }
                 CFRelease(pageCFNumberRef);
-
+                
                 // note: the usage is only valid if the usage page is also defined
                 if (inUsage) {
                     CFNumberRef usageCFNumberRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &inUsage);
-
+                    
                     if (usageCFNumberRef) {
                         if (isDeviceNotElement) {
                             CFDictionarySetValue(result, CFSTR(kIOHIDDeviceUsageKey), usageCFNumberRef);
@@ -120,36 +120,36 @@ static CFMutableDictionaryRef hu_CreateMatchingDictionaryUsagePageUsage(Boolean 
 int main(int argc, const char *argv[]) {
 #pragma unused ( argc, argv )
     IOHIDDeviceRef *tIOHIDDeviceRefs = nil;
-
+    
     // create a IO HID Manager reference
     IOHIDManagerRef tIOHIDManagerRef = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
     require(tIOHIDManagerRef, Oops);
-
+    
     // Create a device matching dictionary
     CFDictionaryRef matchingCFDictRef = hu_CreateMatchingDictionaryUsagePageUsage(TRUE,
-            kHIDPage_GenericDesktop,
-            kHIDUsage_GD_Keyboard);
+                                                                                  kHIDPage_GenericDesktop,
+                                                                                  kHIDUsage_GD_Keyboard);
     require(matchingCFDictRef, Oops);
-
+    
     // set the HID device matching dictionary
     IOHIDManagerSetDeviceMatching(tIOHIDManagerRef, matchingCFDictRef);
-
+    
     if (matchingCFDictRef) {
         CFRelease(matchingCFDictRef);
     }
-
+    
     // Now open the IO HID Manager reference
     IOReturn tIOReturn = IOHIDManagerOpen(tIOHIDManagerRef, kIOHIDOptionsTypeNone);
     require_noerr(tIOReturn, Oops);
-
+    
     // and copy out its devices
     CFSetRef deviceCFSetRef = IOHIDManagerCopyDevices(tIOHIDManagerRef);
     require(deviceCFSetRef, Oops);
-
+    
     // how many devices in the set?
     CFIndex deviceIndex, deviceCount = CFSetGetCount(deviceCFSetRef);
     printf("found %ld devices\n", deviceCount);
-
+    
     // allocate a block of memory to extact the device ref's from the set into
     tIOHIDDeviceRefs = malloc(sizeof(IOHIDDeviceRef) * deviceCount);
     if (!tIOHIDDeviceRefs) {
@@ -157,44 +157,69 @@ int main(int argc, const char *argv[]) {
         deviceCFSetRef = NULL;
         goto Oops;
     }
-
+    
     // now extract the device ref's from the set
     CFSetGetValues(deviceCFSetRef, (const void **) tIOHIDDeviceRefs);
     CFRelease(deviceCFSetRef);
     deviceCFSetRef = NULL;
-
+    
     // before we get into the device loop we'll setup our element matching dictionary
     matchingCFDictRef = hu_CreateMatchingDictionaryUsagePageUsage(FALSE, kHIDPage_LEDs, 0);
     require(matchingCFDictRef, Oops);
-
+    
+    
+    
     int pass;    // do 256 passes
-    int goodpass = 5;
-    for (pass = goodpass; pass < goodpass + 1; pass++) {
+    int start = 0;
+    int end = 256;
+    bool fromFile = false;
+    char buffer[256];
+    
+    
+    
+    FILE *fp = fopen("cmstorm_config.cfg", "r");
+    if (fp) {
+        char *line = NULL;
+        size_t len;
+        ssize_t read = getline(&line, &len, fp);
+        if (read) {
+            fromFile = true;
+            int goodpass = atoi(line);
+            start = goodpass;
+            end = goodpass + 1;
+        }
+        fclose(fp);
+    } else {
+        printf("cmstorm_config.cfg not found. Cycling through possible values.\n");
+    }
+    
+    
+    for (pass = start; pass < end; pass++) {
         Boolean delayFlag = FALSE;    // if we find an LED element we'll set this to TRUE
-
+        
         printf("pass = %d.\n", pass);
         for (deviceIndex = 0; deviceIndex < deviceCount; deviceIndex++) {
-
+            
             // if this isn't a keyboard device...
             if (!IOHIDDeviceConformsTo(tIOHIDDeviceRefs[deviceIndex], kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard)) {
                 continue;    // ...skip it
             }
-
+            
             printf("	 device = %p.\n", tIOHIDDeviceRefs[deviceIndex]);
-
+            
             // copy all the elements
             CFArrayRef elementCFArrayRef = IOHIDDeviceCopyMatchingElements(tIOHIDDeviceRefs[deviceIndex],
-                    matchingCFDictRef,
-                    kIOHIDOptionsTypeNone);
+                                                                           matchingCFDictRef,
+                                                                           kIOHIDOptionsTypeNone);
             if (!elementCFArrayRef) {
                 continue;
             }
-
+            
             // for each device on the system these values are divided by the value ranges of all LED elements found
             // for example, if the first four LED element have a range of 0-1 then the four least significant bits of
             // this value will be sent to these first four LED elements, etc.
             int device_value = pass;
-
+            
             // iterate over all the elements
             CFIndex elementIndex, elementCount = CFArrayGetCount(elementCFArrayRef);
             if (elementCount == 3) { // because the mac internal keyboard has 5 and the CM Storm has 3
@@ -203,33 +228,33 @@ int main(int argc, const char *argv[]) {
                     if (!tIOHIDElementRef) {
                         continue;
                     }
-
+                    
                     uint32_t usagePage = IOHIDElementGetUsagePage(tIOHIDElementRef);
-
+                    
                     // if this isn't an LED element...
                     if (kHIDPage_LEDs != usagePage) {
                         continue;    // ...skip it
                     }
-
+                    
                     uint32_t usage = IOHIDElementGetUsage(tIOHIDElementRef);
                     IOHIDElementType tIOHIDElementType = IOHIDElementGetType(tIOHIDElementRef);
-
+                    
                     printf("		 element = %p (page: %d, usage: %d, type: %d ).\n",
-                            tIOHIDElementRef, usagePage, usage, tIOHIDElementType);
-
+                           tIOHIDElementRef, usagePage, usage, tIOHIDElementType);
+                    
                     // get the logical mix/max for this LED element
                     CFIndex minCFIndex = IOHIDElementGetLogicalMin(tIOHIDElementRef);
                     CFIndex maxCFIndex = IOHIDElementGetLogicalMax(tIOHIDElementRef);
-
+                    
                     // calculate the range
                     CFIndex modCFIndex = maxCFIndex - minCFIndex + 1;
-
+                    
                     // compute the value for this LED element
                     CFIndex tCFIndex = minCFIndex + (device_value % modCFIndex);
                     device_value /= modCFIndex;
-
+                    
                     printf("			 value = 0x%08lX.\n", tCFIndex);
-
+                    
                     uint64_t timestamp = 0; // create the IO HID Value to be sent to this LED element
                     IOHIDValueRef tIOHIDValueRef = IOHIDValueCreateWithIntegerValue(kCFAllocatorDefault, tIOHIDElementRef, timestamp, tCFIndex);
                     if (tIOHIDValueRef) {
@@ -237,35 +262,49 @@ int main(int argc, const char *argv[]) {
                         tIOReturn = IOHIDDeviceSetValue(tIOHIDDeviceRefs[deviceIndex], tIOHIDElementRef, tIOHIDValueRef);
                         CFRelease(tIOHIDValueRef);
                         require_noerr(tIOReturn, next_element);
-                        delayFlag = TRUE;    // set this TRUE so we'll delay before changing our LED values again
+                        delayFlag = TRUE;    // set this TRUE so we'll delay before changing our LED values agai
+                        
                     }
-                    next_element:;
+                next_element:;
                     continue;
                 }
             }
-            CFRelease(elementCFArrayRef);            
+            CFRelease(elementCFArrayRef);
             continue;
         }
-
+        
+        if (!fromFile){
+            printf("is this value good? (y/n)\n");
+            fgets(buffer, sizeof(buffer), stdin);
+//            printf("buffer=`%s`\n", buffer);
+            if (buffer[0] == 'y') {
+                printf("saving configuration\n");
+                fp = fopen("cmstorm_config.cfg", "w");
+                fprintf(fp, "%d\n", pass);
+                fclose(fp);
+                break;
+            }
+        }
+        
         // if we found an LED we'll delay before continuing
-//        if (delayFlag) {
-//            usleep(500000); // sleep one half second
-//        }
-
+        //        if (delayFlag) {
+        //            usleep(500000); // sleep one half second
+        //        }
+        
         // if the mouse is down…
         if (GetCurrentButtonState()) {
             break;    // abort pass loop
         }
     }                          // next pass
-
+    
     if (matchingCFDictRef) {
         CFRelease(matchingCFDictRef);
     }
-    Oops:;
+Oops:;
     if (tIOHIDDeviceRefs) {
         free(tIOHIDDeviceRefs);
     }
-
+    
     if (tIOHIDManagerRef) {
         CFRelease(tIOHIDManagerRef);
     }
